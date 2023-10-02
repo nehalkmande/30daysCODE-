5329ddb15a8826473b618c356813a2bab7aa8b8a
nehalkmande <122994367+nehalkmande@users.noreply.github.com>
2023-10-02 16:46:32 +0530

    Update day28 ques.py

--- Day28/ques.py
+++ Day28/ques.py
@@ -4,14 +4,13 @@ def rotate_array(arr, k):
     # k is within the range [0, n)
     k = k % n
     
-    # rotation using slicing
+    # the rotation using slicing
     rotated_arr = arr[-k:] + arr[:-k]
     
     return rotated_arr
 
-# Example usage:
 original_arr = [1, 2, 3, 4, 5]
-k = 2  # Rotate by 2 times to the right
+k = -4 # Rotate by 2 times to the right (negative value)
 
 rotated_arr = rotate_array(original_arr, k)
 print(rotated_arr)  
